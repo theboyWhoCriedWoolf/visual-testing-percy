807900b02653ccb581882e8ed7d05438edf5f138
Tal Woolf <talwoolf@tals-mbp.home>
2019-12-13 00:41:38 +0000

    d

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  launch: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  },
   server: {
     command: 'cross-env NODE_ENV=production node visual-testing-app/server.js',
     port: 3001,
--- packages/components/Button.js
+++ packages/components/Button.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export default styled.button`
-  background: lilac;
+  background: green;
   color: red;
   padding: 20px;
 `;
