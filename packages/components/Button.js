c8eb01025c8f41aab1612569efcbdcb185de35e1
Tal Woolf <talwoolf@tals-mbp.home>
2019-12-13 00:26:02 +0000

    d

--- packages/components/Button.js
+++ packages/components/Button.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export default styled.button`
-  background: blue;
+  background: lilac;
   color: red;
   padding: 20px;
 `;
--- packages/components/text-input.visualspec.js
+++ packages/components/text-input.visualspec.js
@@ -1,33 +1,9 @@
-import puppeteer from 'puppeteer';
 import { percySnapshot } from '@percy/puppeteer';
 
-const platform = require('os').platform();
-const puppeteerArgs = /^win/.test(platform) ? [] : ['--single-process'];
-
 describe('<Button>', () => {
-  let page;
-  let server;
-  let browser;
-
-  beforeEach(async () => {
-    browser = await puppeteer.launch({
-      headless: true,
-      timeout: 10000,
-      args: puppeteerArgs,
-    });
-
-    page = await browser.newPage();
-
-    await page.goto('http://localhost:3001/Button');
-  });
-
-  afterEach(function() {
-    // Close the Puppeteer browser instance.
-    browser.close();
-  });
-
   it('Default', async () => {
+    await page.goto('http://localhost:3001/Button');
     // await expect(page).toMatch('A label text');
-    await percySnapshot(page, 'Button');
+    await percySnapshot(page, 'Button', { widths: [300] });
   });
 });
